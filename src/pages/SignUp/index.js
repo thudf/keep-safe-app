94b7d578abd1ae5755b7af783077711905aa2743
Arthur D Afonseca <arthur.dafonseca89@gmail.com>
2022-09-07 15:56:20 -0300

    feat: add signup route

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+const Home = () => (
+  <View style={{ flex: 1, backgroundColor: 'blue', justifyContent: 'center', alignItems: 'center' }}>
+    <Text>SignUp</Text>
+  </View>
+);
+
+export default Home;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from '../pages/SignIn';
-// import SignUp from '../pages/SignUp';
+import SignUp from '../pages/SignUp';
 
 const Auth = createStackNavigator();
 
@@ -14,7 +14,7 @@ const AuthRoutes = () => (
     }}
   >
     <Auth.Screen name="SignIn" component={SignIn} />
-    {/* <Auth.Screen name="SignUp" component={SignUp} /> */}
+    <Auth.Screen name="SignUp" component={SignUp} />
   </Auth.Navigator>
 );
 
